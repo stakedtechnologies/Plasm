27c93edbac37984f5b91d3fc6bd2f0da66a51ef6
hskang9 <hskang9@gmail.com>
2021-02-19 13:01:52 +0900

    configure plasm-rewards

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -799,7 +799,7 @@ construct_runtime!(
 
         // Network staking related pallets
         Authorship: pallet_authorship::{Module, Call, Storage, Inherent},
-        PlasmRewards: pallet_plasm_rewards::{Module, Call, Storage, Event<T>, Config},
+        PlasmRewards: pallet_plasm_rewards::{Module, Call, Storage, Event<T>},
         Staking: pallet_plasm_staking::{Module, Call, Storage, Event<T>, Config<T>},
         Treasury: pallet_treasury::{Module, Call, Storage, Config, Event<T>},
         Grandpa: pallet_grandpa::{Module, Call, Storage, Config, Event, ValidateUnsigned},
